8a526560aa76286e845648951b30b11d31df9fbc
andre <aaa.newwork@gmail.com>
2026-06-11 10:25:43 +0300

    delete check zaglushka from urls

--- lawyer/urls.py
+++ lawyer/urls.py
@@ -5,20 +5,23 @@ from lawyer_website import secret
 
 app_name = 'lawyer'
 
-# zaglushka = 1
+urlpatterns = [
+    path('', LawyerHome.as_view(), name='home'),
+]
 
-if secret.zaglushka == 0:
-    urlpatterns = [
-        # path('', index, name='zaglushka'),
-        path('', LawyerHome.as_view(), name='home'),
-        # path('feedback/', FeedbackCreateView.as_view(), name='feedback'),
-    ]
-else:
-    urlpatterns = [
-        path('', index, name='zaglushka'),
-        # path('', LawyerHome.as_view(), name='home'),
-        # path('feedback/', FeedbackCreateView.as_view(), name='feedback'),
-    ]
+
+# if secret.zaglushka == 0:
+#     urlpatterns = [
+#         # path('', index, name='zaglushka'),
+#         path('', LawyerHome.as_view(), name='home'),
+#         # path('feedback/', FeedbackCreateView.as_view(), name='feedback'),
+#     ]
+# else:
+#     urlpatterns = [
+#         path('', index, name='zaglushka'),
+#         # path('', LawyerHome.as_view(), name='home'),
+#         # path('feedback/', FeedbackCreateView.as_view(), name='feedback'),
+#     ]
 
 # urlpatterns = [
 #     # path('', index, name='zaglushka'),
